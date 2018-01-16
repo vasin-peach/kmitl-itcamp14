e306c9a4855258f817a816b2c6f1f0573b5f3324
Peaches <Shinasaki07@gmail.com>
2018-01-16 23:50:07 +0700

    (Source) change dev run at host

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -22,6 +22,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
+    host: 'grabkeys.net',
+    port: 8081,
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
